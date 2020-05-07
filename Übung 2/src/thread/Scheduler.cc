03cb3a274ad13fa0a26afd3eeb64802aeef033b2
schusvin <vincent.schuster.mail@web.de>
2020-05-07 18:22:16 +0200

    implement scheduler

--- Übung 2/src/thread/Scheduler.cc
+++ Übung 2/src/thread/Scheduler.cc
@@ -10,18 +10,24 @@
 
 #include "lib/Queue.h"
 #include "thread/Schedulable.h"
+#include "thread/Scheduler.h"
 
 	// Einfuegen eines neuen Elements in die Ready-Liste.
 	void Scheduler::schedule(Schedulable* sched) {
-	
+        this -> readylist.enqueue(sched);
 	}
 
 	// Entfernen eines Elements von der Ready-Liste.
 	void Scheduler::remove(Schedulable* sched) {
+        this -> readylist.remove(sched);
 		
 	}
 
 	// Aktiviert das vorderste der Liste mittels activate.
 	void Scheduler::reschedule() {
-		
+		Schedulable* firstElement;
+        
+        firstElement = (Schedulable*) this -> readylist.dequeue();
+        
+        activate(firstElement);
 	}
